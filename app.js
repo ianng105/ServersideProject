259416334b68954dbbdfe24409867dc5c488665a
ianng105 <champian.ng@gmail.com>
2025-11-18 11:10:20 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-const app=require("express");
+const express=require("express");
+const app=express();
+PORT=8080
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+
+  });
