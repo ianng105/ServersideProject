74556c28f8050469d68b0ce71376be30da0eb5d3
ianng105 <champian.ng@gmail.com>
2025-11-20 15:34:23 +0800

    Add files via upload

--- model/mongo.js
+++ model/mongo.js
@@ -1,8 +1,8 @@
 const { MongoClient } = require('mongodb');
 
 
-const uri = “mongodb+srv://huil25514_db_user:<1234>@cluster0.96ftjs5.mongodb.net/?appName=Cluster0”
-const client = new MonogoClient(uri);
+const uri = 'mongodb+srv://huil25514_db_user:1234@cluster0.96ftjs5.mongodb.net/?appName=Cluster0';
+const client = new MongoClient(uri);
 
 let db;
 
