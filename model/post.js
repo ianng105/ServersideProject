91ce555a3e24ed13e14f305cd22ad83be39cda53
wangyusen050213-ship-it <wangyusen050213@gmail.com>
2025-11-21 01:21:31 +0800

    Update post.js

--- model/post.js
+++ model/post.js
@@ -1,78 +1,64 @@
 const { MongoClient, ObjectId } = require('mongodb');
-
-// 复用 MongoDB Atlas 连接字符串
-const uri = "mongodb+srv://hui125514_db_user:1234@c1uster0.96ftjs5.mongodb.net/?appName=Cluster0";
-const client = new MongoClient(uri);
-const dbName = "healthypal";
+const{getDB} = require("./mongo");
 const collectionName = "post";
-
+class Post{
 // 连接数据库并获取集合
-async function getCollection() {
-  await client.connect();
-  return client.db(dbName).collection(collectionName);
+  static async  getCollection() {
+      const db=getDB();
+		  return await db.collection(collectionName);
 }
 
 // 创建帖子（关联用户ID）
-async function createPost(postData) {
-  const collection = await getCollection();
-  try {
-    const result = await collection.insertOne(postData);
-    return { ...postData, _id: result.insertedId };
-  } finally {
-    await client.close();
-  }
+   static async  createPost(postData) {
+     const collection =await Post.getCollection();
+     const result = await collection.insertOne(postData);
+     console.log(result);
+     return { ...postData, _id: result.insertedId };
 }
 
 // 查询所有帖子（关联用户信息）
-async function findAllPosts() {
-  const collection = await getCollection();
-  try {
+    static async  findAllPosts() {
+    const collection = await Post.getCollection();
+  
     // 模拟关联查询（原生驱动需手动join，此处简化为查询后前端处理）
     return await collection.find().toArray();
-  } finally {
-    await client.close();
-  }
+ 
 }
 
 // 根据ID查询帖子
-async function findPostById(id) {
-  const collection = await getCollection();
-  try {
+  static async findPostById(id) {
+  const collection = await Post.getCollection();
+  
     return await collection.findOne({ _id: new ObjectId(id) });
-  } finally {
-    await client.close();
-  }
+ 
+  
 }
 
 // 更新帖子
-async function updatePost(id, updateData) {
-  const collection = await getCollection();
-  try {
+  static async updatePost(id, updateData) {
+  const collection = await Post.getCollection();
     const result = await collection.updateOne(
       { _id: new ObjectId(id) },
       { $set: updateData }
     );
     return result.modifiedCount > 0;
-  } finally {
-    await client.close();
-  }
+  
 }
 
 // 删除帖子
-async function deletePost(id) {
-  const collection = await getCollection();
-  try {
+  static async deletePost(id) {
+  const collection = await Post.getCollection();
     const result = await collection.deleteOne({ _id: new ObjectId(id) });
     return result.deletedCount > 0;
-  } finally {
-    await client.close();
+  
   }
+
 }
 
 module.exports = {
-  createPost,
-  findAllPosts,
-  findPostById,
-  updatePost,
-  deletePost
+  createPost:Post.createPost,
+  findAllPosts:Post.findAllPosts,
+  findPostById:Post.findPostById,
+  updatePost:Post.updatePost,
+  deletePost:Post.deletePost,
 };
