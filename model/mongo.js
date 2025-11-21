7f9a0e98f1910a5f8b4aeb307294531df9eec281
ianng105 <champian.ng@gmail.com>
2025-11-21 21:43:42 +0800

    Add files via upload

--- model/mongo.js
+++ model/mongo.js
@@ -1,27 +1,36 @@
 const { MongoClient } = require('mongodb');
 
-
+// 1. 连接字符串
 const uri = 'mongodb+srv://huil25514_db_user:1234@cluster0.96ftjs5.mongodb.net/?appName=Cluster0';
+
+// 2. 创建 MongoClient 实例
 const client = new MongoClient(uri);
 
+// 3. 声明 db 变量，用于存储数据库实例
 let db;
 
+// 4. 定义并立即导出 connectDB 函数
 async function connectDB() {
   try {
     await client.connect();
+    // 连接成功后，指定要使用的数据库
     db = client.db('healthypal');
     console.log('Connected to MongoDB');
     return db;
   } catch (err) {
     console.error('DB connection error:', err);
-    throw err;
+    throw err; // 抛出错误，让调用方知道连接失败
   }
 }
 
+// 5. 定义并立即导出 getDB 函数
 function getDB() {
+  if (!db) {
+    throw new Error('Database not connected. Call connectDB first.');
+  }
   return db;
 }
 
-
+// 6. 导出模块
 module.exports = { connectDB, getDB };
 
--- model/user.js
+++ model/user.js
@@ -1,67 +1,54 @@
 const { MongoClient, ObjectId } = require('mongodb');
-const{getDB} = require("./mongo");
+const { getDB } = require("./mongo");
 const collectionName = "user";
-class User{
 
-	// 连接数据库并获取集合
-	static async getCollection() {
-		
-		  const db=getDB();
-		  return await db.collection(collectionName);
-		
+class User {
+  // 连接数据库并获取集合
+  static async getCollection() {
+    const db = getDB();
+    return await db.collection(collectionName);
+  }
 
-	}
+  // 注册用户
+  static async createUser(userData) {
+    const collection = await User.getCollection();
+    const result = await collection.insertOne(userData);
+    return { ...userData, _id: result.insertedId };
+  }
 
-	// 注册用户
-	static async createUser(userData) {
-		
-		    const collection = await User.getCollection();
-		    const result = await collection.insertOne(userData);
-		    console.log(result);
-		    return { ...userData, _id: result.insertedId };
-		
+  // 查询所有用户
+  static async findAllUsers() {
+    const collection = await User.getCollection();
+    return await collection.find().toArray();
+  }
 
-	}
+  // 根据ID查询用户 - 修复版
+  static async findUserById(id) {
+    const collection = await User.getCollection();
+    return await collection.findOne({ _id: new ObjectId(id) });
+  }
 
-	// 查询所有用户
-	static async findAllUsers() {
-		  const collection = await User.getCollection().find().toArray();
-		  return collection;
-	}
+  static async findUserByUsername(email) {
+    const collection = await User.getCollection();
+    return await collection.findOne({ email });
+  }
 
-	// 根据ID查询用户
-	static async findUserById(id) {
-			const collection = await User.getCollection();	
-			return await collection.find().toArray();
-	  			
-	}
+  // 更新用户信息
+  static async updateUser(id, updateData) {
+    const collection = await User.getCollection();
+    const result = await collection.updateOne(
+      { _id: new ObjectId(id) },
+      { $set: updateData }
+    );
+    return result.modifiedCount > 0;
+  }
 
-	// 根据用户名查询用户（用于登录验证）
-	static async  findUserByUsername(username) {
-			const collection = await User.getCollection();
-	  		return await collection.findOne({ username });
-	}
-
-	// 更新用户信息
-	static async  updateUser(id, updateData) {
-		    const collection = await User.getCollection();
-		    const result = await collection.updateOne(
-		      { _id: new ObjectId(id) },
-		      { $set: updateData }
-		    );
-		    return result.modifiedCount > 0;
-	}
-
-	// 删除用户
-	static async  deleteUser(id) {
-		
-		    const collection = await User.getCollection();
-		    const result = await collection.deleteOne({ _id: new ObjectId(id) });
-		    return result.deletedCount > 0;
-		
-	    
-	  
-	}
+  // 删除用户
+  static async deleteUser(id) {
+    const collection = await User.getCollection();
+    const result = await collection.deleteOne({ _id: new ObjectId(id) });
+    return result.deletedCount > 0;
+  }
 }
 
 module.exports = {
